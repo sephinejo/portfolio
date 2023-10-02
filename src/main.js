d2c17e53beddfdbfa22b2d2148fcf186d053a734
SooHyun-Kang <k.josephine19@gmail.com>
2023-10-02 11:24:43 -0700

    javascript home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-console.log(headerHeight);
-
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
