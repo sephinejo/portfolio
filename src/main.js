5801029ab8c78a8320bdff9e9a571ce11f84eff2
SooHyun-Kang <k.josephine19@gmail.com>
2023-10-02 12:57:25 -0700

    javascript menu

--- src/main.js
+++ src/main.js
@@ -13,3 +13,22 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+const arrowup = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY < homeHeight / 2) {
+    arrowup.style.opacity = 0;
+  } else {
+    arrowup.style.opacity = 1;
+  }
+});
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+});
