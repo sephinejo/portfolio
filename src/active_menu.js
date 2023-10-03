847d49e9212885b98a6b644c72013e283ca4fc99
SooHyun-Kang <k.josephine19@gmail.com>
2023-10-02 20:32:43 -0700

    javascript scroll smooth

--- src/active_menu.js
+++ src/active_menu.js
@@ -21,7 +21,7 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
 
     const navIndex = selectLastOne
       ? sectionIds.length - 1
