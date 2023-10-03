4bef6cb62813cfb0e154749130daa1bf93c623ce
SooHyun-Kang <k.josephine19@gmail.com>
2023-10-02 20:28:46 -0700

    javascript menu highlight

--- src/active_menu.js
+++ src/active_menu.js
@@ -4,6 +4,7 @@ const navItems = sectionIds.map((id) =>
   document.querySelector(`[href='${id}']`)
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
 const options = {
   rootMargin: '-20% 0px 0px 0px',
@@ -21,10 +22,24 @@ function observerCallback(entries) {
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
       entry.intersectionRatio >= 0.99;
+
+    const navIndex = selectLastOne
+      ? sectionIds.length - 1
+      : findFirstIntersecting(visibleSections);
+
+    selectNavItem(navIndex);
   });
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 }
